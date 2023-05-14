04dc710caa2c1b379e50f4f420314f11fb7fe8d2
Kaseke <kasymkhankhubievnis@gmail.com>
2023-05-14 12:27:05 +0700

    made docs

---
+++ helper.py
@@ -0,0 +1,10 @@
+# знак файла:
+is_file_sign = ' (f)'
+
+# знак директории
+is_dir_sign = ' (d)'
+
+# new line
+new_line = '\n'
+
+#
--- show-tree.py
+++ show-tree.py
@@ -1,10 +1,20 @@
 import os
 import sys
 
+from helper import is_file_sign, is_dir_sign, new_line
 # сообщение об ошибке
 exception_message = 'Only directories'
+
+# отступ первого элемента в каталоге
 first_sign = '`-- '
+
+# отступ второго элемента в каталоге
 other_sign = '|-- '
+
+# отступ для жлементова подкаталогов
+subdir_sign = '|   '
+
+# отступ для элементов подкаталога
 delta = '    '
 
 def is_path_to_file(path: str) -> None:
@@ -12,54 +22,107 @@ def is_path_to_file(path: str) -> None:
     Функция проверяет указывает ли введенный адрес на директорию.
     Если указывает на директорию - вызываем исключение,
     иначе - ничего не делаем.
+
+    Аргументы:
+        path, string - путь к директории/файлу
     """
     if os.path.isfile(path):
+        # бросаем исключение с ошибкой - выполнение сразу прерывается
         raise Exception(f'{exception_message}')
     
+    
 def pars_directory(path: str, deep: int, parent_is_first: bool) -> str:
-    has_first = False
-    out_put = ''
+    """
+    В данной функции реализуем чтение и запись дерева каталога.
+    
+    Получаем путь к директории, получаем список содержимого этой директории.
+    Пробегаемся по всем элементам.
+    Если попадается файл - просто записываем файл.
+    Если директория - рекурсивно проходим до конца ветви - конец ветви: пустая директория или
+                      директория, содержащая только файлы.
 
+    Аргументы:
+        path, string - путь к директории
+        deep, integer - глубина текущей ветви дерева
+        parent_is_first, bool - указывает, является ли родительская 
+            директория первой в своей директории. это нужно для того,
+            чтобы правильно устанавливать отступы для элементов подкаталогов.
+
+    Возвращаемое значение:
+        dir_tree, str - дерево каталога в виде строки.
+    
+    """
+    dir_tree = ''
+
+    # получаем список файлов и каталогов внутри текущего каталога
     _dir_inner = os.listdir(path)
+
+    # если директория пустая - конец, иначе нужно пробежаться
     if len(_dir_inner) != 0:
-        for item in _dir_inner:
+
+        # enumerate возвращается кортеж (пару значений): 
+        # idx - индекс (порядковый номер) элемента массива и 
+        # item - элемент массива соответствующий индексу
+        for idx, item in enumerate(_dir_inner):
             line = ''
+
+            # Если родительская директория не была первой в своем списке
+            # нужно вдоль вертикали устанавливать отступ '|   '.
             if parent_is_first == False:
                 line += '|   '
+
+            # проверяем является ли элемент в списке файлом посредством функции: os.path.isfile(path),
+            # если файл - возвращается True, иначе False
+            # os.path.join(path, file) - конкатенирует путь и название файла.
             if os.path.isfile(os.path.join(path, item)):
-                if has_first == False:
-                    has_first = True
-                    if parent_is_first:
-                        out_put += line + delta * deep + first_sign + item + ' (f)' + '\n'
+                if idx == 0: # если первый в списке директории
+                    if parent_is_first: # Если родительская директория тоже первая, ничего лишнего ставить не нужно
+                        dir_tree += line + delta * deep + first_sign + item + is_file_sign + '\n'
+                    else: # а если не первая то по уровню родительской вдоль вертикали надо спец отступ ставить
+                        dir_tree += delta * (deep - 1) + line + first_sign + item + is_file_sign + '\n'
+                else: # не первый в списке
+                    if parent_is_first: # аналогично
+                        dir_tree += delta * deep + line + other_sign + item + is_file_sign + '\n'
                     else:
-                        out_put += delta * (deep - 1) + line + first_sign + item + ' (f)' + '\n'
-                else:
-                    if parent_is_first:
-                        out_put += delta * deep + line + other_sign + item + ' (f)' + '\n'
-                    else:
-                        out_put += delta * (deep - 1) + line + other_sign + item + ' (f)' + '\n'
-            elif os.path.isdir(os.path.join(path, item)):
-                if has_first == False:
-                    has_first = True
-                    if parent_is_first:
-                        out_put += delta * deep + line + first_sign + item + ' (d)' + '\n'
-                        out_put += pars_directory(path=os.path.join(path, item), deep=deep+1, parent_is_first=True)
-                    else:
-                        out_put += delta * (deep - 1) + line + first_sign + item + ' (d)' + '\n'
+                        dir_tree += delta * (deep - 1) + line + other_sign + item + is_file_sign + '\n'
+
+            elif os.path.isdir(os.path.join(path, item)): # является ли директорией, как в случае с файлом
+                if idx == 0: # если первый в списке директории
+                    if parent_is_first: # если родительская директория тоже первая 
+                        dir_tree += delta * deep + line + first_sign + item + is_dir_sign + '\n'
+                        # идем глубже в директории
+                        dir_tree += pars_directory(path=os.path.join(path, item), deep=deep+1, parent_is_first=True)
+                    else: # директория не первая в списке
+                        dir_tree += delta * (deep - 1) + line + first_sign + item + is_dir_sign + '\n'
                         
+                        # идем глубже в директорию - не забываем установить 'parent_is_first=True', т.к. idx = 0
                         out = pars_directory(path=os.path.join(path, item), deep=deep+1, parent_is_first=True)
+
+                        '''
+                        т.к. текущая директория не первая, все последующие файлы/директории
+                        должны быть помечены отступом с вертикальной чертой на уровне объявления директории
+
+                        пример: 
+                            Dir
+                            `-- first_dir (d)
+                            |-- second_dir (d)
+                            |   `-- first_subdir (d)
+                            |   |-- second_subdir (d)
+                            |   |-- file (f)
+                        '''
+                        # разрезаем out на строки
                         lines = out.split('\n')
                         for line in lines:
-                            if len(line) > 0:
-                                out_put += line[:(deep-1)*4] + '|   ' + line[deep*4 - 1:] + '\n'
-                else:
+                            if len(line) > 0: # пустую строку в конце надо выкинуть
+                                dir_tree += line[:(deep-1)*4] + '|   ' + line[deep*4 - 1:] + '\n'
+                else: # не первый в списке 
                     if parent_is_first:
-                        out_put += delta * deep + line + other_sign + item + '(d)' + '\n'
+                        dir_tree += delta * deep + line + other_sign + item + is_dir_sign + '\n'
                     else:
-                        out_put += delta * (deep - 1) + line + other_sign + item + '(d)' + '\n'
-                    out_put += pars_directory(path=os.path.join(path, item), deep=deep+1, parent_is_first=False)
+                        dir_tree += delta * (deep - 1) + line + other_sign + item + is_dir_sign + '\n'
+                    dir_tree += pars_directory(path=os.path.join(path, item), deep=deep+1, parent_is_first=False)
 
-    return out_put
+    return dir_tree
 
 def main() -> None:
 
